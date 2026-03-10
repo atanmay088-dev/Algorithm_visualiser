a9df5094e688461f2a3a451b2c931edd5be41c2d
atanmay088-dev <atanmay088@gmail.com>
2026-03-10 21:41:07 +0530

    added a skip button , completed bubblesorting algorithm , still issues reamain

--- logic.js
+++ logic.js
@@ -1,5 +1,6 @@
 // list containing all ids of bars
 let barID = ["0","1","2","3","4","5","6","7","8"];
+let remoteAbort = null;
 // simply generating random heights for bars
 function randomHeight(min,max){
     return Math.floor(Math.random()*(max - min) + min);
@@ -15,26 +16,97 @@ function setBarHeight(idOfBar , heightToSet){
     document.getElementById(idOfBar).style.height = heightToSet;
 }
 function heightOfBar(IdOfBar){
-    return document.getElementById(IdOfBar);
+    return document.getElementById(IdOfBar).style.height ;
 }
 //creating timer
-function delay(time){
-   return new Promise(resolve => setTimeout(1000));
+function delay(time , signal){
+   return new Promise((resolve,reject) => {
+    if (signal?.aborted){
+        return reject(new DOMException("Aborted" , "abortError"));
+    }
+    let timeoutId = setTimeout(resolve , time)});
+    signal?.addEventListener('abort' , () => {
+        clearTimeout(timeoutId);
+        reject(new DOMException("aborted" , "abortError"));
+    } )
 }
-function bubbleSort(){
+function changeColorNew(block1 , block2){
+    document.getElementById(block1).style.backgroundColor = `#445e93ff`;
+    document.getElementById(block2).style.backgroundColor = `#445e93ff`;
+}
+function changeColorBack(block1 , block2){
+    document.getElementById(block1).style.backgroundColor = `#fcecc9ff`;
+    document.getElementById(block2).style.backgroundColor = `#fcecc9ff`;
+}
+function heightSwap( block1 , block2 ){
+    let temp1 = heightOfBar(block1);
+    let temp2 = heightOfBar(block2);
+    setBarHeight(block1 , temp2);
+    setBarHeight(block2 , temp1);
+}
+function bubbleSortSkip(){
     let numberOfBars = barID.length;
     for(let i = 0 ; i < numberOfBars ; i++){
-        for(let j = i ; j < numberOfBars ; j++){
+        for(let j = i+1 ; j < numberOfBars ; j++){
             if (heightOfBar(barID[i]) > heightOfBar(barID[j])){
-                let temp1 = heightOfBar(barID[i]);
-                let temp2 = heightOfBar(barID[j]);
-                setBarHeight(barID[i] , temp2);
-                setBarHeight(barID[j] , temp1);
+                heightSwap(barID[i] , barID[j]);
             }
         }
     }
 }
-function main(){
-    barInitialisation()
+async function bubbleSort(signal){
+    try{
+        let numberOfBars = barID.length;
+        for(let i = 0 ; i < numberOfBars ; i++){
+            for(let j = i+1 ; j < numberOfBars ; j++){
+                if (heightOfBar(barID[i]) > heightOfBar(barID[j])){
+                    await delay(1000 , signal);
+                    changeColorNew(barID[i] , barID[j]);
+                    await delay(3000 , signal);
+                    heightSwap(barID[i] , barID[j]);
+                    await delay(3000 , signal);
+                    changeColorBack(barID[i] , barID[j]);
+                    await delay(1000 , signal);
+                }
+                else{
+                    await delay(1000 , signal);
+                    changeColorNew(barID[i] , barID[j]);
+                    await delay(3000 , signal);
+                    changeColorBack(barID[i] , barID[j]);
+                    await delay(1000 , signal);
+
+                }
+            }
+        }
+    }
+    catch (err){
+        if (err.name == "abortError"){
+            console.log("Sorting aborted");
+        }
+        else{
+            throw err;
+        }
+    }
+}
+async function main(){
+    barInitialisation();
+    let startButton = document.getElementById('bubbleSort');
+    let skipButton = document.getElementById('reset');
+    startButton.addEventListener ( 'click' , async () => {
+        if (remoteAbort){
+            remoteAbort.abort();
+        }
+        remoteAbort = new AbortController();
+        startButton.disabled = 'true';
+        await bubbleSort(remoteAbort.signal);
+        startButton.disabled = 'false';
+    });
+    skipButton.addEventListener('click' , () => {
+        if (remoteAbort){
+            remoteAbort.abort();
+        }
+        bubbleSortSkip()
+        startButton.disabled = 'false';
+    });
 }
 window.addEventListener('DOMContentLoaded' , (event) => main());
