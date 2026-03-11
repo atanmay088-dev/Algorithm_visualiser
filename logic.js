9c680475b6682ddc90e566e9a5417d5d151bd88b
atanmay088-dev <atanmay088@gmail.com>
2026-03-11 22:07:55 +0530

    added insertion and selection sort , still bugs remain

--- logic.js
+++ logic.js
@@ -24,19 +24,42 @@ function delay(time , signal){
     if (signal?.aborted){
         return reject(new DOMException("Aborted" , "abortError"));
     }
-    let timeoutId = setTimeout(resolve , time)});
+    let timeoutId = setTimeout(resolve , time);
     signal?.addEventListener('abort' , () => {
         clearTimeout(timeoutId);
+        for(i = 0 ; i < barID.length-1 ; i++){
+            changeColorBack(barID[i] , barID[i+1]);
+        }
         reject(new DOMException("aborted" , "abortError"));
-    } )
+    }) 
+    })
+}
+function sortedPartOfSelectionSort(start , end){
+    for( let i = start ; i <= end ; i++){
+        document.getElementById(barID[i]).style.backgroundColor = 'black';
+    }
+}
+function setAllDefaultColor(){
+    let allBar = document.querySelectorAll('.box');
+    allBar.forEach((bar) => {
+        bar.style.backgroundColor = `#fcecc9ff`;
+    });
 }
 function changeColorNew(block1 , block2){
-    document.getElementById(block1).style.backgroundColor = `#445e93ff`;
-    document.getElementById(block2).style.backgroundColor = `#445e93ff`;
+    if (block1){
+        document.getElementById(block1).style.backgroundColor = `#445e93ff`;
+    }
+    if (block2){
+        document.getElementById(block2).style.backgroundColor = `#445e93ff`;
+    }
 }
 function changeColorBack(block1 , block2){
-    document.getElementById(block1).style.backgroundColor = `#fcecc9ff`;
-    document.getElementById(block2).style.backgroundColor = `#fcecc9ff`;
+    if (block1){
+        document.getElementById(block1).style.backgroundColor = `#fcecc9ff`;
+    }
+    if (block2){
+        document.getElementById(block2).style.backgroundColor = `#fcecc9ff`;
+    }
 }
 function heightSwap( block1 , block2 ){
     let temp1 = heightOfBar(block1);
@@ -44,13 +67,74 @@ function heightSwap( block1 , block2 ){
     setBarHeight(block1 , temp2);
     setBarHeight(block2 , temp1);
 }
-function bubbleSortSkip(){
+function skipSort(){
     let numberOfBars = barID.length;
     for(let i = 0 ; i < numberOfBars ; i++){
         for(let j = i+1 ; j < numberOfBars ; j++){
             if (heightOfBar(barID[i]) > heightOfBar(barID[j])){
                 heightSwap(barID[i] , barID[j]);
             }
+        }
+    }
+}
+async function insertionSort(signal){
+    try{
+        for( let i = 1 ; i < barID.length ; i++){
+            sortedPartOfSelectionSort(0,i);
+            await delay(2000 , signal);
+            let key = document.getElementById(barID[i]).style.height;
+            let j = i - 1;
+            while( j >= 0 && heightOfBar(barID[j]) > key ){
+                setBarHeight(barID[j+1] , document.getElementById(barID[j]).style.height)
+                j--;
+            }   
+            setBarHeight(barID[j+1] , key);
+        }
+        setAllDefaultColor();
+    } 
+    catch (err){
+        if (err.name == "abortError"){
+            console.log("Sorting aborted");
+        }
+        else{
+            throw err;
+        }
+    }
+}
+async function selectionSort(signal){
+    try{
+        for (let i = 0 ; i < barID.length ; i++){
+            let idx = barID[i];
+            for ( let j = i+1 ; j < barID.length ; j++){
+                let OGIndex = null;
+                await delay(2000 , signal);
+                changeColorNew(barID[j] , idx);
+                await delay(2000 , signal);
+                if (heightOfBar(idx) > heightOfBar(barID[j])){
+                    OGIndex = idx ;
+                    idx = barID[j];
+                }
+                if (OGIndex){
+                    changeColorBack(OGIndex , null);
+                }
+                else{
+                    changeColorBack(null  , barID[j]);
+                }
+            }
+            await delay(2000 , signal);
+            changeColorNew(idx , barID[i]);
+            await delay(2000 , signal);
+            heightSwap(idx , barID[i]);
+            await delay(2000 , signal);
+            changeColorBack(idx , barID[i]);
+        }
+    }
+    catch (err){
+        if (err.name == "abortError"){
+            console.log("Sorting aborted");
+        }
+        else{
+            throw err;
         }
     }
 }
@@ -90,23 +174,62 @@ async function bubbleSort(signal){
 }
 async function main(){
     barInitialisation();
-    let startButton = document.getElementById('bubbleSort');
+    let allSortButtons = document.querySelectorAll('.sortButtons');
+    let bubbleSortButton = document.getElementById('bubbleSort');
+    let selectionSortButton = document.getElementById('selectionSort');
+    let insertionSortButton = document.getElementById('insertionSort'); 
     let skipButton = document.getElementById('reset');
-    startButton.addEventListener ( 'click' , async () => {
+    bubbleSortButton.addEventListener ( 'click' , async () => {
         if (remoteAbort){
             remoteAbort.abort();
         }
         remoteAbort = new AbortController();
-        startButton.disabled = 'true';
+        document.body.classList.add('sortingInProgress');
+        allSortButtons.forEach((button) => {
+            button.disabled = true;
+        });  
         await bubbleSort(remoteAbort.signal);
-        startButton.disabled = 'false';
+        allSortButtons.forEach((button) => {
+            button.disabled = false;
+        });
     });
     skipButton.addEventListener('click' , () => {
         if (remoteAbort){
             remoteAbort.abort();
         }
-        bubbleSortSkip()
-        startButton.disabled = 'false';
+        skipSort()
+        document.body.classList.remove('sortingInProgress');
+        allSortButtons.forEach((button) => {
+            button.disabled = false;
+        });
+    });
+    selectionSortButton.addEventListener ( 'click' , async () => {
+        if (remoteAbort){
+            remoteAbort.abort();
+        }
+        remoteAbort = new AbortController();
+        document.body.classList.add('sortingInProgress');
+        allSortButtons.forEach((button) => {
+            button.disabled = true;
+        }); 
+        await selectionSort(remoteAbort.signal);
+        allSortButtons.forEach((button) => {
+            button.disabled = false;
+        });
+    });
+    insertionSortButton.addEventListener ( 'click' , async () => {
+        if (remoteAbort){
+            remoteAbort.abort();
+        }
+        remoteAbort = new AbortController();
+        document.body.classList.add('sortingInProgress');
+        allSortButtons.forEach((button) => {
+            button.disabled = true;
+        }); 
+        await insertionSort(remoteAbort.signal);
+        allSortButtons.forEach((button) => {
+            button.disabled = false;
+        });
     });
 }
 window.addEventListener('DOMContentLoaded' , (event) => main());
